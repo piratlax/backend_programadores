7d3dc528cba717d2a28b8a036753284e0d8aacaf
Mao Rivera <maorivera@outlook.com>
2023-07-14 12:35:47 -0600

    Agregado logout y create usuarios

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Laravel\Sanctum\Sanctum;
 
 class AuthController extends Controller
 {
@@ -37,5 +38,37 @@ class AuthController extends Controller
                 "token" => $token,
             ]
         );
+    }
+    public function signup(): JsonResponse
+    {
+        request()->validate([
+            "name" => "required|min:2|max:60",
+            "email" => "required|email|unique:users",
+            "password" => "required|min:8|max:30",
+            "passwordConfirmation" => "required|same:password|min:8|max:30",
+        ]);
+        User::create([
+            "name" => request("name"),
+            "email" => request("email"),
+            "password" => bcrypt(request("password")),
+            "create_at" => now(),
+        ]);
+        return $this->success(
+            __("Cuenta creada")
+        );
+    }
+    public function logout(): JsonResponse
+    {
+        $token = request()->bearerToken();
+        $model = Sanctum::$personalAccessTokenModel;
+        $accessToken = $model::findToken($token);
+        if ($accessToken) {
+            $accessToken->delete();
+            return $this->success(
+                __("Hasta la proxima"),
+                data: null,
+                code: 204,
+            );
+        }
     }
 }
